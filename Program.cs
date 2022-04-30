1832293024f7a2000663b505287f862952800ec0
Corentin Altepe <corentin.altepe@gmail.com>
2022-04-30 11:41:31 +0200

    Started configuring the push request to send a log to loki.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿Console.WriteLine("Hello World");
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Logproto;
+using static Logproto.Pusher;
 
-//var client = Pusher.Push(null);
+Console.WriteLine("Hello World");
+
+using var channel = GrpcChannel.ForAddress("http://localhost:9095");
+var client = new PusherClient(channel);
+
+
+var query = new PushRequest();
+var response = client.Push(query, new CallOptions());
+Console.WriteLine("End of program: " + response);
