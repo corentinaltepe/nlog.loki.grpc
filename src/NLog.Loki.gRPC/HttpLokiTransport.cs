1e52d4d3b2bc7f0a580b5801c84dc042f1864937
Corentin Altepe <corentin.altepe@gmail.com>
2022-05-26 13:43:02 +0200

    Moved program source code to the HttpLokiTransport class.

--- src/NLog.Loki.gRPC/HttpLokiTransport.cs
+++ src/NLog.Loki.gRPC/HttpLokiTransport.cs
@@ -4,8 +4,13 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Logproto;
 using NLog.Common;
 using NLog.Loki.gRPC.Model;
+using static Logproto.Pusher;
+using System;
 
 namespace NLog.Loki;
 
@@ -32,9 +37,30 @@ internal sealed class HttpLokiTransport : ILokiTransport
 
     public async Task WriteLogEventsAsync(IEnumerable<LokiEvent> lokiEvents)
     {
+        // this is only a proof of concept. It will have to be modified before releasing a production ready package.
+        using var channel = GrpcChannel.ForAddress("http://localhost:9095");
+        var client = new PusherClient(channel);
+        var stream = new StreamAdapter() { Labels = "{level=\"info\",server=\"corentin\",location=\"here\"}" };
+        var now = new Google.Protobuf.WellKnownTypes.Timestamp();
+        now.Seconds = (long)ConvertToUnixTimestamp(DateTime.Now);
+        stream.Entries.Add(new EntryAdapter() { Line = "This is my log 1", Timestamp = now });
+        stream.Entries.Add(new EntryAdapter() { Line = "This is my log 2", Timestamp = now });
+        stream.Entries.Add(new EntryAdapter() { Line = "This is my log 3", Timestamp = now });
+        var query = new PushRequest();
+        query.Streams.Add(stream);
+        var response2 = client.Push(query, new CallOptions());
+        // end of grpc poc
+
         using var jsonStreamContent = CreateContent(lokiEvents);
         using var response = await _lokiHttpClient.PostAsync("loki/api/v1/push", jsonStreamContent).ConfigureAwait(false);
         await ValidateHttpResponse(response).ConfigureAwait(false);
+    }
+
+    static double ConvertToUnixTimestamp(DateTime date)
+    {
+        var origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        var diff = date.ToUniversalTime() - origin;
+        return Math.Floor(diff.TotalSeconds);
     }
 
     public async Task WriteLogEventsAsync(LokiEvent lokiEvent)
